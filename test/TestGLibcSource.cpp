2b6196cf122b7460d3e8f79d25c4d010b8e3135d
Layton <caolei6767@gmail.com>
2021-05-04 14:01:31 +0800

    初始化仓库

---
+++ test/TestGLibcSource.cpp
@@ -0,0 +1,11 @@
+#include "catch2/catch.hpp"
+#include "gnu/libc-version.h"
+#include "spdlog/spdlog.h"
+
+
+TEST_CASE("Debug Glibc")
+{
+    int* intPtr = static_cast<int*>(malloc(sizeof(int)));
+    spdlog::info("Ptr info is {}", fmt::ptr(intPtr));
+    spdlog::info("Glibc Version is {}", gnu_get_libc_version());
+}
