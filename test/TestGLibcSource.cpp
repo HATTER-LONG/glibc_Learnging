2dbde41e707b8b7bb05bd8e79bf279198f94591e
Layton <caolei6767@gmail.com>
2021-05-05 19:36:06 +0800

    完善文档

--- test/TestGLibcSource.cpp
+++ test/TestGLibcSource.cpp
@@ -6,6 +6,7 @@
 TEST_CASE("Debug Glibc")
 {
     int* intPtr = static_cast<int*>(malloc(sizeof(int)));
+    *intPtr = 20;
     spdlog::info("Ptr info is {}", fmt::ptr(intPtr));
     spdlog::info("Glibc Version is {}", gnu_get_libc_version());
 }
