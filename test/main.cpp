4c2b05665203495e7167cea3a467570815e8f022
Layton <caolei6767@gmail.com>
2021-08-16 21:52:37 +0800

    增加多线程验证

--- test/main.cpp
+++ test/main.cpp
@@ -1,13 +1,23 @@
 
+#include "ThreadPool.h"
 #include "fastbin.h"
 #include "tcache.h"
+#include "assert.h"
 
 #include <cstdio>
 #include <cstdlib>
+
+void allocThreadMem()
+{
+    tcacheAllocFreeTest();
+    fastBinAllocFreeTest();
+}
 
 
 int main(void)
 {
     tcacheAllocFreeTest();
     fastBinAllocFreeTest();
+    ThreadPool thread(1);
+    thread.enqueue(allocThreadMem);
 }
